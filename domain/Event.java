f2e67b903284e8531a892d9f1c23b2fc03de3e57
USUARIO <michael3135056851@gmail.com>
2025-11-03 19:54:56 -0500

    Se crean los métodos de la clase Event (pendiente de pruebas)

--- domain/Event.java
+++ domain/Event.java
@@ -1,3 +1,50 @@
+import java.util.List;
+
 public class Event {
+    private int eventId;
+    private String eventName;
+    private String eventDate;
+    private int eventTime;
+    private List<String> eventLocations;
+    private List<Integer> eventCapacityByLocations;
+    private String eventType;
+    private Venue venue;
+    private List<Ticket> tickets;
+
+    public Event(int eventId, String eventName, String eventDate, int eventTime, String eventType){
+        this.eventId = eventId;
+        this.eventName = eventName;
+        this.eventDate = eventDate;
+        this.eventTime = eventTime;
+        this.eventType = eventType;
+    }
+
+    public void setEventName(String newEventName){
+        if(!newEventName.equals("")){
+            eventName = newEventName;
+        }
+    }
     
+    public void setEventDate(String newEventDate){
+        if(!newEventDate.equals("")){
+            eventDate = newEventDate;
+        }
+    }
+
+    public void setEventTime(int newEventTime){
+        if(newEventTime > 0 && newEventTime < 2400){
+            eventTime = newEventTime;
+        }
+    }
+
+    public void setEventType(String newEventType){
+        if(!newEventType.equals("")){
+            eventType = newEventType;
+        }
+    }
+
+    public String getEventName(){return eventName;}
+    public String getEventDate(){return eventDate;}
+    public int getEventTime(){return eventTime;}
+    public String getEventType(){return eventType;}
 }
